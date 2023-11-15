9bf03733c896a081c17f7ece8486ea288f17c5a3
vuduan2004 <121173637+vuduan2004@users.noreply.github.com>
2023-11-15 20:06:06 +0700

    Project_DuAn

---
+++ TechStore_DuAn_Nhom8/app/src/main/java/duanvdph37524/fpoly/techstore_duan_nhom8/MainActivity.java
@@ -0,0 +1,14 @@
+package duanvdph37524.fpoly.techstore_duan_nhom8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
