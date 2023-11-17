ebdcbce23efe84abe35c3f6fcd12e1af3092566c
NguyenDuyPhong1204 <nguyenduyphong12122004@gmail.com>
2023-11-17 14:28:38 +0700

    Activity + Firebase

---
+++ TechStore_DuAn_Nhom8/app/src/main/java/duanvdph37524/fpoly/techstore_duan_nhom8/Activity/DangNhap.java
@@ -0,0 +1,16 @@
+package duanvdph37524.fpoly.techstore_duan_nhom8.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import duanvdph37524.fpoly.techstore_duan_nhom8.R;
+
+public class DangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
